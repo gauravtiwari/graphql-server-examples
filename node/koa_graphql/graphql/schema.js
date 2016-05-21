d18a2ac9a620dd5d88cb2fac2a8353bf3873a37a
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-21 16:40:11 +0100

    Define basic schema

--- node/koa_graphql/graphql/schema.js
+++ node/koa_graphql/graphql/schema.js
@@ -1,3 +1,35 @@
-var Schema = {};
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: globalIdField('User'),
+    first_name: {
+      type: GraphQLString,
+      resolve: (obj) => obj.first_name,
+    },
+    last_name: {
+      type: GraphQLString,
+      resolve: (obj) => obj.last_name,
+    },
+    username: {
+      type: GraphQLString,
+      resolve: (obj) => obj.username,
+    },
+    email: {
+      type: GraphQLString,
+      resolve: (obj) => obj.email,
+    },
+  },
+});
+
+const Root = new GraphQLObjectType({
+  name: 'Root',
+  fields: {
+  },
+});
+
+
+var schema = new GraphQLSchema({
+  query: Root,
+});
 
 module.exports = Schema;
