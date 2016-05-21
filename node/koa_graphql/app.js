41a7bdfe5447e4a262ebfdb6da11b8587c77db50
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-21 15:30:29 +0100

    Generate koa app

---
+++ node/koa_graphql/app.js
@@ -0,0 +1,37 @@
+'use strict';
+const messages = require('./controllers/messages');
+const compress = require('koa-compress');
+const logger = require('koa-logger');
+const serve = require('koa-static');
+const route = require('koa-route');
+const koa = require('koa');
+const path = require('path');
+var mount = require('koa-mount');
+var convert = require('koa-convert');
+var graphqlHTTP = require('koa-graphql');
+
+const app = module.exports = koa();
+
+
+app.use(mount('/graphql', convert(graphqlHTTP({ schema: Schema, graphiql: true }))));
+
+// Logger
+app.use(logger());
+
+app.use(route.get('/', messages.home));
+app.use(route.get('/messages', messages.list));
+app.use(route.get('/messages/:id', messages.fetch));
+app.use(route.post('/messages', messages.create));
+app.use(route.get('/async', messages.delay));
+app.use(route.get('/promise', messages.promise));
+
+// Serve static files
+app.use(serve(path.join(__dirname, 'public')));
+
+// Compress
+app.use(compress());
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log('listening on port 3000');
+}
---
+++ node/koa_graphql/graphql/schema.js
@@ -0,0 +1,3 @@
+var Schema = {};
+
+module.exports = Schema;
