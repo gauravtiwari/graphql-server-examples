21a0c2e128d53c64834fc15565ea0f2b83b92b92
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-22 17:52:27 +0100

    Cleanup comments

--- node/koa_graphql/graphql/schema.js
+++ node/koa_graphql/graphql/schema.js
@@ -20,7 +20,6 @@ let Schema = new GraphQLSchema({
     fields: () => ({
       post: {
         type: PostType,
-        // args will automatically be mapped to `where`
         args: {
           id: {
             description: 'id of the post',
@@ -30,14 +29,11 @@ let Schema = new GraphQLSchema({
         resolve: (obj, { id, ...args }) => Post.forge({ id: id }).fetch({withRelated: ['comments', 'user', 'comments.user'], debug: true}).then((post) => post.toJSON())
       },
       allPosts: {
-        // The resolver will use `findOne` or `findAll` depending on whether the field it's used in is a `GraphQLList` or not.
         type: new GraphQLList(PostType),
         args: {
-          // An arg with the key limit will automatically be converted to a limit on the target
           limit: {
             type: GraphQLInt
           },
-          // An arg with the key order will automatically be converted to a order on the target
           order: {
             type: GraphQLString
           }
