f4092a1891abae90def5939b02e8b9d5f718a102
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-22 17:49:43 +0100

    Update schema

--- node/koa_graphql/graphql/schema.js
+++ node/koa_graphql/graphql/schema.js
@@ -1,35 +1,52 @@
-const UserType = new GraphQLObjectType({
-  name: 'User',
-  fields: {
-    id: globalIdField('User'),
-    first_name: {
-      type: GraphQLString,
-      resolve: (obj) => obj.first_name,
-    },
-    last_name: {
-      type: GraphQLString,
-      resolve: (obj) => obj.last_name,
-    },
-    username: {
-      type: GraphQLString,
-      resolve: (obj) => obj.username,
-    },
-    email: {
-      type: GraphQLString,
-      resolve: (obj) => obj.email,
-    },
-  },
-});
+import Post from '../models/post';
 
-const Root = new GraphQLObjectType({
-  name: 'Root',
-  fields: {
-  },
-});
+import PostType from './types/postType';
 
 
-var schema = new GraphQLSchema({
-  query: Root,
+import {
+  GraphQLBoolean,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
+
+let Schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: () => ({
+      post: {
+        type: PostType,
+        // args will automatically be mapped to `where`
+        args: {
+          id: {
+            description: 'id of the post',
+            type: new GraphQLNonNull(GraphQLInt)
+          }
+        },
+        resolve: (obj, { id, ...args }) => Post.forge({ id: id }).fetch({withRelated: ['comments', 'user', 'comments.user'], debug: true}).then((post) => post.toJSON())
+      },
+      allPosts: {
+        // The resolver will use `findOne` or `findAll` depending on whether the field it's used in is a `GraphQLList` or not.
+        type: new GraphQLList(PostType),
+        args: {
+          // An arg with the key limit will automatically be converted to a limit on the target
+          limit: {
+            type: GraphQLInt
+          },
+          // An arg with the key order will automatically be converted to a order on the target
+          order: {
+            type: GraphQLString
+          }
+        },
+        resolve: (obj, { id, ...args }) => Post.forge().fetchAll({withRelated: ['comments', 'user', 'comments.user'], debug: true}).then((posts) => posts.toJSON())
+      }
+    }),
+  })
 });
+
 
 module.exports = Schema;
