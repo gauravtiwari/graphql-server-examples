3fb7a21452443b9db0df8c8a4ef72ec67a1730ea
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-24 14:02:16 +0100

    Add graphql types and schema

---
+++ ruby/rails_graphql/app/api/types/user_type.rb
@@ -0,0 +1,12 @@
+UserType = GraphQL::ObjectType.define do
+  name "User"
+  description "A user"
+
+  field :id, !types.ID
+  field :first_name, !types.String
+  field :last_name, !types.String
+  field :username, !types.String
+  field :email, !types.String
+  field :posts, types[!PostType]
+  field :comments, types[!CommentType]
+end
