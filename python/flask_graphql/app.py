5bfa4d8651d6c770edf4beedbafd5593c9a08586
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-24 18:39:55 +0100

    Import schema and mount to graphql

--- python/flask_graphql/app.py
+++ python/flask_graphql/app.py
@@ -1,8 +1,19 @@
-from flask import Flask
-
-from database import db_session, init_db
-from flask_graphql import GraphQL
 from schema import schema
+from models import app
+from flask_graphql import GraphQLView
 
-app = Flask(__name__)
-app.debug = True
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+
+if __name__ == '__main__':
+    app.run()
