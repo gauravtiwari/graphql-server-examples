1e290d4d8407e09a6523a811cd19d5ea2b606736
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-24 16:56:23 +0100

    Setup endpoint for graphql and editor

--- ruby/sinatra_graphql/sinatra_graphql.rb
+++ ruby/sinatra_graphql/sinatra_graphql.rb
@@ -1,8 +1,21 @@
 class SinatraGraphql < Sinatra::Base
-
   set :public_folder => "public", :static => true
 
   get "/" do
     erb :welcome
+  end
+
+  get "/graphiql" do
+    erb :graphiql
+  end
+
+  post "/graphql" do
+    params =  JSON.parse(request.body.read)
+    result = Schema.execute(
+      params["query"],
+      variables: params["variables"]
+    )
+    content_type :json
+    result.to_json
   end
 end
