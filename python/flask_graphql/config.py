3517e35b15a60f834d91ffd781a01d265609763a
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-24 18:40:53 +0100

    Add config based on env

---
+++ python/flask_graphql/config.py
@@ -0,0 +1,28 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'some-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
