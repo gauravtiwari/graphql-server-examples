af1ad020e3ef7ed4c4878b3d23c699251489b921
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-25 03:02:52 +0100

    Mount graphql editor to root path

--- python/flask_graphql/app.py
+++ python/flask_graphql/app.py
@@ -2,18 +2,7 @@ from schema import schema
 from models import app
 from flask_graphql import GraphQLView
 
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
-
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
+app.add_url_rule('/', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
 if __name__ == '__main__':
     app.run()
