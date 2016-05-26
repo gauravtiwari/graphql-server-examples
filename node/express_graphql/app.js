0bf51e88087119462aa1c0da1522c949cd213ba5
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-26 04:36:45 +0100

    Clean app.js file

--- node/express_graphql/app.js
+++ node/express_graphql/app.js
@@ -5,21 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var graphqlHTTP = require('express-graphql');
-var graphqlSchema = require('./graphql/schema');
+var graphqlSchema = require('./api/schema');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', graphqlHTTP({ schema: graphqlSchema, graphiql: true }));
 
 
