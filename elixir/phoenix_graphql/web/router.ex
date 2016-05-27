3703b87a9cb808387bbea643a9a99952058653fd
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-27 15:57:16 +0100

    Add route for editor and mount schema

--- elixir/phoenix_graphql/web/router.ex
+++ elixir/phoenix_graphql/web/router.ex
@@ -1,11 +1,29 @@
 defmodule PhoenixGraphql.Router do
   use PhoenixGraphql.Web, :router
 
-  pipeline :api do
-    plug :accepts, ["json"]
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
   end
 
-  scope "/api", PhoenixGraphql do
-    pipe_through :api
+  pipeline :graphql do
+    plug :accepts, ["json"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug PhoenixGraphql.Context
+  end
+
+  scope "/", PhoenixGraphql do
+    pipe_through :browser
+    get "/", PageController, :graphiql
+  end
+
+  scope "/graphql" do
+    pipe_through :graphql
+    get "/", Absinthe.Plug, schema: PhoenixGraphql.Api.Schema
+    post "/", Absinthe.Plug, schema: PhoenixGraphql.Api.Schema
   end
 end
