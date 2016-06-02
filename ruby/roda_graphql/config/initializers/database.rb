a68b780de2f7c0a4da85b7339b0290bc9ab2b823
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-06-02 13:23:51 +0100

    Use dotenv

--- ruby/roda_graphql/config/initializers/database.rb
+++ ruby/roda_graphql/config/initializers/database.rb
@@ -1,4 +1,5 @@
 require "yaml"
+Dotenv.load
 settings = YAML::load_file("config/db.yml")
 # Sequel Configuration
-DB = Sequel.connect(settings[ENV['RACK_ENV']])
+DB = Sequel.connect(settings[ENV['ENVIRONMENT']])
