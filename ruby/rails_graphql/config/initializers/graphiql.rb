b2c001c6622e5eb6a3479448a372010ae69923ae
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-06-04 03:14:58 +1200

    Enable csrf for rails

---
+++ ruby/rails_graphql/config/initializers/graphiql.rb
@@ -0,0 +1,3 @@
+GraphiQL::Rails.config.query_params = false # if true, the GraphQL query string will be persisted the page's query params.
+GraphiQL::Rails.config.initial_query = GraphiQL::Rails::WELCOME_MESSAGE # This string is presented to a new user
+GraphiQL::Rails.config.csrf = true
