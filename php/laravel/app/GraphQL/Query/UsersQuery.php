db083af0d8448da9ac0d4a3e06e54ddc0c6d40f3
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-06-03 16:05:21 +0100

    Add users query

---
+++ php/laravel/app/GraphQL/Query/UsersQuery.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\GraphQL\Query;
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Query;
+use App\User;
+
+class UsersQuery extends Query {
+
+  protected $attributes = [
+    'name' => 'Users query'
+  ];
+
+  public function type()
+  {
+    return Type::listOf(GraphQL::type('user'));
+  }
+
+  public function args()
+  {
+    return [
+      'id' => ['name' => 'id', 'type' => Type::string()]
+    ];
+  }
+
+  public function resolve($root, $args)
+  {
+    if(isset($args['id']))
+    {
+      return User::where('id' , $args['id'])->get();
+    }
+    else if(isset($args['email']))
+    {
+      return User::where('email', $args['email'])->get();
+    }
+    else
+    {
+      return User::all();
+    }
+  }
+
+}
