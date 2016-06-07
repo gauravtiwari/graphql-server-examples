26f85e50e71ad98b85a37e5c238df676ff3ae8e0
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-06-07 15:37:23 +0100

    Add token auth for elixir app

--- elixir/phoenix_graphql/web/controllers/page_controller.ex
+++ elixir/phoenix_graphql/web/controllers/page_controller.ex
@@ -1,7 +1,15 @@
 defmodule PhoenixGraphql.PageController do
   use PhoenixGraphql.Web, :controller
+  import Joken
 
   def graphiql(conn, _params) do
-    render(conn, "graphiql.html")
+    # Pass to root route as we don't know the token
+    # For real API you would use authentication or something
+    auth_token =  token()
+      |> with_sub(1234567890)
+      |> sign(hs256("secret"))
+      |> get_compact
+
+    render(conn, "graphiql.html", token: auth_token)
   end
 end
--- elixir/phoenix_graphql/web/router.ex
+++ elixir/phoenix_graphql/web/router.ex
@@ -1,5 +1,8 @@
 defmodule PhoenixGraphql.Router do
   use PhoenixGraphql.Web, :router
+  import Joken
+
+  @skip_token_verification %{joken_skip: true}
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -13,17 +16,26 @@ defmodule PhoenixGraphql.Router do
     plug :accepts, ["json"]
     plug :fetch_session
     plug :fetch_flash
+    plug Joken.Plug, verify: &PhoenixGraphql.Router.verify_function/0
     plug PhoenixGraphql.Context
   end
 
+  # Skip root route
   scope "/", PhoenixGraphql do
     pipe_through :browser
-    get "/", PageController, :graphiql
+    get "/", PageController, :graphiql, private: @skip_token_verification
   end
 
   scope "/graphql" do
     pipe_through :graphql
     get "/", Absinthe.Plug, schema: PhoenixGraphql.Api.Schema
     post "/", Absinthe.Plug, schema: PhoenixGraphql.Api.Schema
+  end
+
+  def verify_function() do
+    %Joken.Token{}
+    |> with_json_module(Poison)
+    |> with_signer(hs256("secret"))
+    |> with_sub(1234567890)
   end
 end
