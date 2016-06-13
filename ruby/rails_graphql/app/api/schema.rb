90d155534b978eaf67baf2dfcefaf7eca0938264
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-06-13 16:12:26 +0100

    Fix typo

--- ruby/rails_graphql/app/api/schema.rb
+++ ruby/rails_graphql/app/api/schema.rb
@@ -5,7 +5,7 @@ QueryType = GraphQL::ObjectType.define do
   field :post do
     type PostType
     argument :id, !types.ID
-    resolve -> (obj, args, ctx) { Post.find(args["id"]).includes(:user, :comments) }
+    resolve -> (obj, args, ctx) { Post.includes(:user, :comments).find(args["id"]) }
   end
 
   field :all_posts do
